74edadc9afe360d8c1739ba5e80be7e93288bbe5
Peter <petedianotech@gmail.com>
2026-01-01 22:31:40 +0000

    Add Monetag Vignette Banner ad to my game
    - Place the ad between game

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,9 +47,15 @@ export default function RootLayout({
             })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
           `}
         </Script>
+        <Script id="monetag-vignette" strategy="afterInteractive">
+          {`
+            (function(s){
+              s.dataset.zone='10405020';
+              s.src='https://gizokraijaw.net/vignette.min.js';
+            })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+          `}
+        </Script>
       </body>
     </html>
   );
 }
-
-    
