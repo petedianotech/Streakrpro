7885a1b7f3fc19bd513e8406259b417a2760f3c0
Peter <petedianotech@gmail.com>
2026-01-15 16:02:48 +0000

    another issue ads only show when I open the app on appsike Facebook, tik

--- src/components/AdScripts.tsx
+++ src/components/AdScripts.tsx
@@ -1,9 +1,38 @@
-
 'use client';
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 export function AdScripts() {
+
+    useEffect(() => {
+        const initAdsgram = () => {
+            if ((window as any).Adsgram && (window as any).Adsgram.init) {
+                console.log('Adsgram SDK loaded, initializing...');
+                (window as any).Adsgram.init({ blockId: '477' });
+            } else {
+                console.log('Adsgram SDK not found yet, will retry...');
+            }
+        };
+
+        // Check if Adsgram is already loaded
+        if ((window as any).Adsgram) {
+            initAdsgram();
+        }
+        
+        // If not, set up a listener for when the script loads
+        const script = document.querySelector('script[src="https://tg.adsgram.app/js/adsgram.js"]');
+        if (script) {
+            script.addEventListener('load', initAdsgram);
+        }
+
+        return () => {
+            if (script) {
+                script.removeEventListener('load', initAdsgram);
+            }
+        };
+    }, []);
+
     return (
         <>
             {/* Monetag Vignette Banner for ads between page loads */}
@@ -15,15 +44,12 @@ export function AdScripts() {
                     })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
                 `}
             </Script>
-            {/* Adsgram script for Rewarded Video Ads */}
+
+            {/* Adsgram script for Rewarded Video Ads - onLoad is removed */}
             <Script
+                id="adsgram-sdk"
                 src="https://tg.adsgram.app/js/adsgram.js"
                 strategy="afterInteractive"
-                onLoad={() => {
-                    if ((window as any).Adsgram) {
-                        (window as any).Adsgram.init({ blockId: '477' });
-                    }
-                }}
             />
         </>
     );
